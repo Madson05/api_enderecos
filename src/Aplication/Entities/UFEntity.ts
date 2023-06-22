81a831e269683e7ec16a2eb48b1dc47eaa6da257
madson <madsonsilva2005@gmail.com>
2023-06-22 20:15:16 -0300

    feat: adicionada entidade UFEntity para criação das UFS

---
+++ src/Aplication/Entities/UFEntity.ts
@@ -0,0 +1,19 @@
+class UFEntity{
+
+
+
+    constructor(
+        private id: number,
+        private nome: string,
+        private sigla: string,
+        private status: number
+    ){
+        this.id = id;
+        this.nome = nome;
+        this.sigla = sigla;
+        this.status = status;
+    }
+
+}
+
+export default UFEntity;
