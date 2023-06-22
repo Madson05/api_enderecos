44fe5f5b22c4d000c7d4101b87600fae56977b85
madson <madsonsilva2005@gmail.com>
2023-06-22 20:41:51 -0300

    feat: iniciado roteamento da aplicação através de implementação do roteamento da UF (separação de responsabilidade entre cada rota de cada entidade)

---
+++ src/Aplication/Routes/Router.ts
@@ -0,0 +1,8 @@
+import {Router} from "express"
+import routerUF from "./UF.route";
+
+const router = Router();
+
+router.use("/uf", routerUF)
+
+export default router;
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
+import router from "./Aplication/Routes/Router";
 
 dotenv.config();
 
 const app = express();
+
+app.use("/", router);
 
 app.listen(process.env.APLICATION_PORT, () => {
     console.log(`Server is running on port ${process.env.APLICATION_PORT}`);
