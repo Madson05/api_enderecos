7b85156c511b9144d27d6f2c010f23eeb0edc770
madson <madsonsilva2005@gmail.com>
2023-06-23 14:36:22 -0300

    feat: adicionado getters e setters para os atributos de UF. (Finalizado o encapsulamento da entidade)

--- src/Aplication/Entities/UFEntity.ts
+++ src/Aplication/Entities/UFEntity.ts
@@ -10,5 +10,33 @@ class UFEntity{
         this.sigla = sigla;
         this.status = status;
     }
+
+    public getCodigoUF(): number{
+        return this.codigoUF;
+    }
+
+    public getSigla(): string{
+        return this.sigla;
+    }
+
+    public getNome(): string{
+        return this.nome;
+    }
+
+    public getStatus(): number{
+        return this.status;
+    }
+
+    public setSigla(sigla: string): void{
+        this.sigla = sigla;
+    }
+
+    public setNome(nome: string): void{
+        this.nome = nome;
+    }
+
+    public setStatus(status: number): void{
+        this.status = status;
+    }
 }
 export default UFEntity;
