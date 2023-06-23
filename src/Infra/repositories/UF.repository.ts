a1af1b598001c757f4663b65e019137b0407085e
madson <madsonsilva2005@gmail.com>
2023-06-23 14:48:48 -0300

    fix: resolvido problemas ao passar os parametron na execução da querySQL na criação de UF

--- src/Infra/repositories/UF.repository.ts
+++ src/Infra/repositories/UF.repository.ts
@@ -4,8 +4,13 @@ import getConnection from "../database/connectionDB";
 class UFRepository {
     async create(UF: UFEntity) {
         const connection = await getConnection();
-        const sql = `INSERT INTO UF (id, name,) VALUES (:id, :nome, :sigla, :status)`;
-        const result = await connection.execute(sql, UF);
+        const sql = `INSERT INTO UF (codigoUF, nome, sigla, status) VALUES (:codigoUF, :nome, :sigla, :status)`;
+        const result = connection.execute(sql, {
+            codigoUF: UF.getCodigoUF(),
+            nome: UF.getNome(),
+            sigla: UF.getSigla(),
+            status: UF.getStatus()
+          });
         return result;
     }
 }
