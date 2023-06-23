5d810367843bd80071f14c047ecd45e1c62b6959
madson <madsonsilva2005@gmail.com>
2023-06-23 16:15:14 -0300

    feat: implementado instancia do createUfController no roteamento da aplicação

--- src/Aplication/Routes/UF.route.ts
+++ src/Aplication/Routes/UF.route.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import CreateUFController from "../Domains/UF/UseCases/CreateUF.controller";
+import { createUFController } from "../Domains/UF/UseCases/CreateUF.factory";
 
 const routerUF = Router();
 
-routerUF.post("/", CreateUFController.handle)
+routerUF.post("/", createUFController.handle)
 
 export default routerUF;
 
