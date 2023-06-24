3395b0cad3845d8a82e2d2a4ea50199e761803df
madson <madsonsilva2005@gmail.com>
2023-06-24 18:02:26 -0300

    feat: feito o roteamento e do getUf

--- src/Aplication/Routes/UF.route.ts
+++ src/Aplication/Routes/UF.route.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import CreateUFController from "../Domains/UF/UseCases/Create/CreateUF.controller";
 import { createUFController } from "../Domains/UF/UseCases/Create/CreateUF.factory";
+import { getUFController } from "../Domains/UF/UseCases/Get/GetUF.factory";
 
 const routerUF = Router();
 
+routerUF.get("/", getUFController.handle)
 routerUF.post("/", createUFController.handle)
 
 export default routerUF;
