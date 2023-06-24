607e4d11ba6b2fceb9d9f40696ba7220879b925e
madson <madsonsilva2005@gmail.com>
2023-06-24 11:50:50 -0300

    feat: implementado fechamento da conexão após a execução de criação da UF.

--- src/Infra/repositories/UF.repository.ts
+++ src/Infra/repositories/UF.repository.ts
@@ -2,20 +2,27 @@ import UFEntity from "../../Aplication/Entities/UFEntity";
 import getConnection from "../database/connectionDB";
 
 class UFRepository {
-    async create(UF: UFEntity) {
-        const connection = await getConnection();
-        const sql = `INSERT INTO TB_UF (codigo_UF, sigla, nome, status) VALUES (:codigoUF, :sigla, :nome, :status)`;
-        const result = await connection.execute(sql, 
-            {
-            codigoUF: UF.getCodigoUF(),
-            sigla: UF.getSigla(),
-            nome: UF.getNome(),
-            status: UF.getStatus()
-          }
-          );
-        await connection.commit();
-        return result;
+  async create(UF: UFEntity) {
+    let connection;
+    try {
+      connection = await getConnection();
+      const sql = `INSERT INTO TB_UF (codigo_UF, sigla, nome, status) VALUES (:codigoUF, :sigla, :nome, :status)`;
+      const result = await connection.execute(sql, {
+        codigoUF: UF.getCodigoUF(),
+        sigla: UF.getSigla(),
+        nome: UF.getNome(),
+        status: UF.getStatus(),
+      });
+      await connection.commit();
+      return result;
+    } catch (error) {
+      throw new Error("Não foi possivel criar a UF");
+    } finally {
+      if (connection) {
+        await connection.close();
+      }
     }
+  }
 }
 
 export default UFRepository;
