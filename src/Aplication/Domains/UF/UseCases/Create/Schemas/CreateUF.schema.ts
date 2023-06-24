f17c826ae6b33f8013569c3eb778f4b416aea871
madson <madsonsilva2005@gmail.com>
2023-06-24 17:11:25 -0300

    refactor: refatoradas as validações para melhor adequação coma as regras do banco de dados

--- src/Aplication/Domains/UF/UseCases/Create/Schemas/CreateUF.schema.ts
+++ src/Aplication/Domains/UF/UseCases/Create/Schemas/CreateUF.schema.ts
@@ -1,9 +1,18 @@
 import zod from "zod";
 
 export const CreateUFSchema = zod.object({
-    sigla: zod.string().min(1, "A sigla da UF deve ter no minimo 1 caracter").max(256, "A sigla da UF deve ter no maximo 256 caracteres"),
-    nome: zod.string().min(2, "O nome da UF deve ter no minimo 2 caracteres").max(256, "Seu nome deve ter no maximo 256 caracteres"),
-    status: zod.number().min(0, "O status da UF deve ser 0 ou 1").max(1, "O status da UF deve ser 0 ou 1")
-})
+  sigla: zod
+    .string()
+    .max(3, "A sigla da UF deve ter no maximo 3 caracteres")
+    .nonempty("A sigla da UF não pode ser vazia"),
+  nome: zod
+    .string()
+    .max(60, "O nome da UF deve ter no maximo 60 caracteres")
+    .nonempty("O nome da UF não pode ser vazio"),
+  status: zod
+    .number()
+    .min(0, "O status da UF deve ser 0 ou 1")
+    .max(1, "O status da UF deve ser 0 ou 1"),
+});
 
 export type CreateUFType = zod.infer<typeof CreateUFSchema>;
