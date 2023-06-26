9fefb24deb533a2be9fed470bb8dd9030458f6b0
madson <madsonsilva2005@gmail.com>
2023-06-26 16:03:13 -0300

    feat: adicionada rota para edição de uf

--- src/Aplication/Routes/UF.route.ts
+++ src/Aplication/Routes/UF.route.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { createUFController } from "../Domains/UF/UseCases/Create/CreateUF.factory";
 import { getUFController } from "../Domains/UF/UseCases/Get/GetUF.factory";
+import { updateUFController } from "../Domains/UF/UseCases/Update/UpdateUF.factory";
 
 const routerUF = Router();
 
 routerUF.get("/", getUFController.handle)
 routerUF.post("/", createUFController.handle)
+routerUF.put("/", updateUFController.handle)
 
 export default routerUF;
 
