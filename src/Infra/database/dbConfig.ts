7fc39bb01981893c832312e15c679c7cbcf03dd5
madson <madsonsilva2005@gmail.com>
2023-06-26 12:22:32 -0300

    feat: armazenados dados sensiveis no .env para maior segurança da aplicação

--- src/Infra/database/dbConfig.ts
+++ src/Infra/database/dbConfig.ts
@@ -1,9 +1,13 @@
 import oracledb from 'oracledb';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const dbConfig: oracledb.ConnectionAttributes = {
-  user: 'C##NODE',
-  password: 'node',
-  connectString: 'localhost:1521/xe'
+  user: `${process.env.DATABASE_APLICATION_USER}`,
+  password: `${process.env.DATABASE_APLICATION_PASSWORD}`,
+  connectString: `${process.env.DATABASE_APLICATION_HOST}:${process.env.DATABASE_PORT}/${process.env.DATABASE_APLICATION_SID}`
+
 };
 
 export default dbConfig;
