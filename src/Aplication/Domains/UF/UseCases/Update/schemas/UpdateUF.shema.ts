d6f96d1305281778480958954054a91662a8c1eb
madson <madsonsilva2005@gmail.com>
2023-06-28 09:40:27 -0300

    fix: criado schema exclusivo para update para evitar conflitos na aplicação

--- src/Aplication/Domains/UF/UseCases/Update/schemas/UpdateUF.shema.ts
+++ src/Aplication/Domains/UF/UseCases/Update/schemas/UpdateUF.shema.ts
@@ -2,7 +2,10 @@ import { GetUFSchema } from "../../Get/schemas/getUF.schema";
 import zod from "zod"
 
 export const updateUFSchema = zod.object({
-  ...GetUFSchema.shape,
+  codigo_UF: zod.number(),
+  sigla: zod.string(),
+  nome: zod.string(),
+  status: zod.number(),
 })
 
 export type UpdateUFType = zod.infer<typeof updateUFSchema>
