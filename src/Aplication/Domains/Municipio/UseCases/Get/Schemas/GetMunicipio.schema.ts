e7cece8f53dac83ffc6540ebd4bf204a9dba5f6f
madson <madsonsilva2005@gmail.com>
2023-06-28 17:13:07 -0300

    feat: adicionado schema de validação de input para a funcionalidade de getMunicipio

---
+++ src/Aplication/Domains/Municipio/UseCases/Get/Schemas/GetMunicipio.schema.ts
@@ -0,0 +1,10 @@
+import zod from "zod";
+
+export const getMunicipioSchema = zod.object({
+  codigo_municipio: zod.string().optional(),
+  codigo_UF: zod.string().optional(),
+  nome: zod.string().optional(),
+  status: zod.number().optional(),
+});
+
+export type GetMunicipioType = zod.infer<typeof getMunicipioSchema>;
