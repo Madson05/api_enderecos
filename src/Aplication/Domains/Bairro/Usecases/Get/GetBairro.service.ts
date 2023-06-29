1045e91e95a694094f1a7d2041cd81708cda741d
madson <madsonsilva2005@gmail.com>
2023-06-29 10:13:23 -0300

    feat: adicionado service do getBairro

--- src/Aplication/Domains/Bairro/Usecases/Get/GetBairro.service.ts
+++ src/Aplication/Domains/Bairro/Usecases/Get/GetBairro.service.ts
@@ -1,1 +1,32 @@
-class 
+import BairroRepository from "../../../../../Infra/repositories/Bairro.repository";
+import { refactorResult } from "../../Util/RefactorResult";
+import { GetBairroType } from "./Schema/GetBairro.schema";
+
+type GetBairroTypeService = GetBairroType & {
+  [key: string]: number | string | undefined;
+}
+
+class GetBairroService{
+  constructor(private bairroRepository: BairroRepository) {}
+
+  async execute(data: GetBairroTypeService): Promise<any> {
+    let query = "";
+
+    for (const item in data) {
+      if (data.hasOwnProperty(item)) {
+        if (item !== undefined) {
+          if (query !== "") {
+            query += " AND ";
+          }
+          query += `${item}='${data[item]}'`;
+        }
+      }
+
+      const result = await this.bairroRepository.get(query);
+      return refactorResult(result, data);
+    }
+  }
+
+}
+
+export default GetBairroService;
