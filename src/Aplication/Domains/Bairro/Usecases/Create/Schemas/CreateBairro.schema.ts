655c9cd223a5e00365339358d68c325a68670315
madson <madsonsilva2005@gmail.com>
2023-06-29 20:23:48 -0300

    fix: alterado nome do schema de criação

---
+++ src/Aplication/Domains/Bairro/Usecases/Create/Schemas/CreateBairro.schema.ts
@@ -0,0 +1,9 @@
+import zod from "zod";
+
+export const CreateBairroSchema = zod.object({
+  codigoBairro: zod.number().min(1).max(999999),
+  nome: zod.string().min(1).max(100),
+  status: zod.number().min(1).max(1),
+});
+
+export type GetBairroType = zod.infer<typeof CreateBairroSchema>;
