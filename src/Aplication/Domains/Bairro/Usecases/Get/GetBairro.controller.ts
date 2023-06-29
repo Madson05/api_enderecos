8f02c21621876fcf7f869762b0dd54f2711a8216
madson <madsonsilva2005@gmail.com>
2023-06-29 10:29:24 -0300

    fix: corrigido problema no acesso aos dados da request no getBairro

--- src/Aplication/Domains/Bairro/Usecases/Get/GetBairro.controller.ts
+++ src/Aplication/Domains/Bairro/Usecases/Get/GetBairro.controller.ts
@@ -6,7 +6,7 @@ class GetBairroController{
   constructor (private readonly getBairroService: GetBairroService) {}
 
   handle = async (request: Request, response: Response): Promise<any> => {
-    const data: GetBairroType = GetBairroSchema.parse(request.body);
+    const data: GetBairroType = GetBairroSchema.parse(request.query);
     response.status(200).send(await this.getBairroService.execute(data));
   }
 }
