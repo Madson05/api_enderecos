b7a77ba31f10babef5c6bb862c4f3d4921d642fe
madson <madsonsilva2005@gmail.com>
2023-06-29 16:53:37 -0300

    fix: tornadas querys params como opcional no schema

--- src/Aplication/Domains/Bairro/Usecases/Get/Schema/GetBairro.schema.ts
+++ src/Aplication/Domains/Bairro/Usecases/Get/Schema/GetBairro.schema.ts
@@ -1,10 +1,10 @@
 import zod from "zod";
 
 export const GetBairroSchema = zod.object({
-  codigo_bairro: zod.string().min(1),
-  codigo_municipio: zod.string().min(1).max(999999999),
-  nome: zod.string().min(1).max(100),
-  status: zod.string().min(0).max(1),
+  codigo_bairro: zod.string().min(1).optional(),
+  codigo_municipio: zod.string().min(1).max(999999999).optional(),
+  nome: zod.string().min(1).max(100).optional(),
+  status: zod.string().min(0).max(1).optional(),
 });
 
 export type GetBairroType = zod.infer<typeof GetBairroSchema>;
