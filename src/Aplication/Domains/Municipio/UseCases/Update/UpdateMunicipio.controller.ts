fa66d4bcbecf12fdab849e07fce2a655ef296893
madson <madsonsilva2005@gmail.com>
2023-06-29 08:53:20 -0300

    feat: adicionado controller de atualização de municipio

---
+++ src/Aplication/Domains/Municipio/UseCases/Update/UpdateMunicipio.controller.ts
@@ -0,0 +1,16 @@
+import { Response } from "express";
+import UpdateMunicipioService from "./UpdateMunicipio.service";
+import { UpdateMunicipioSchema } from "./schemas/UpdateMunicipio.schema";
+
+class UpdateMunicipioController{
+  constructor(private readonly updateMunicipioService: UpdateMunicipioService) {}
+
+  async handle(request: Request, response: Response): Promise<any> {
+
+    const municipio = UpdateMunicipioSchema.parse(request.body);
+
+    return await this.updateMunicipioService.execute(municipio);
+  }
+}
+
+export default UpdateMunicipioController;
