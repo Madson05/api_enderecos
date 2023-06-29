c901123a74577f69698bde300bb6112040a711b4
madson <madsonsilva2005@gmail.com>
2023-06-29 09:20:02 -0300

    corrigido problema na validação dos dados de updateMunicipio

--- src/Aplication/Domains/Municipio/UseCases/Update/schemas/UpdateMunicipio.schema.ts
+++ src/Aplication/Domains/Municipio/UseCases/Update/schemas/UpdateMunicipio.schema.ts
@@ -2,7 +2,7 @@ import zod from "zod";
 
 export const UpdateMunicipioSchema = zod.object({
   codigo_municipio: zod.number().min(1),
-  codigo_UF: zod.number().min(1).max(2),
+  codigo_UF: zod.number().min(1).max(999999999),
   nome: zod.string().min(1).max(100),
   status: zod.number().min(0).max(1),
 });
