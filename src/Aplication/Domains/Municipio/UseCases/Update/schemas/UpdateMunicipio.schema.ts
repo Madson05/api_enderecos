58584ad610f0be92b4de1baf336191152144d3d8
madson <madsonsilva2005@gmail.com>
2023-06-29 09:05:42 -0300

    fix: atualizado shema de atualização de municipio

--- src/Aplication/Domains/Municipio/UseCases/Update/schemas/UpdateMunicipio.schema.ts
+++ src/Aplication/Domains/Municipio/UseCases/Update/schemas/UpdateMunicipio.schema.ts
@@ -1,9 +1,10 @@
 import zod from "zod";
 
 export const UpdateMunicipioSchema = zod.object({
+  codigo_municipio: zod.number().min(1),
   codigo_UF: zod.number().min(1).max(2),
   nome: zod.string().min(1).max(100),
-  status: zod.string().min(1).max(1),
+  status: zod.number().min(0).max(1),
 });
 
 export type UpdateMunicipioType = zod.infer<typeof UpdateMunicipioSchema>;
