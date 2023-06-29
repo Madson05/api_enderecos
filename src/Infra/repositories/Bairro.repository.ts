a52d3d887dc2d2817eeb6fbed815af4e98f07f38
madson <madsonsilva2005@gmail.com>
2023-06-29 09:54:58 -0300

    feat: adicionado repository para Bairro e metodo de listagem no mesmo

---
+++ src/Infra/repositories/Bairro.repository.ts
@@ -0,0 +1,25 @@
+import BairroEntity from "../../Aplication/Entities/BairroEntity";
+import getConnection from "../database/connectionDB";
+
+class BairroRepository{
+  async get(query: string){
+    let connection;
+    try{
+      connection = await getConnection();
+      if (query === "" || query === undefined){
+        const result = await connection.execute(`SELECT * FROM TB_BAIRRO`);
+        return result.rows;
+      }
+      
+      const sql = `SELECT * FROM TB_BAIRRO WHERE ${query}`;
+      const result = await connection.execute(sql);
+      await connection.commit();
+      return result.rows;
+    
+    }finally{
+      if(connection){
+        await connection.close();
+      }
+    }
+  }
+}
