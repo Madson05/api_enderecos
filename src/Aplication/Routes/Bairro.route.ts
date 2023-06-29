11d19cbde2c506664cf7a1400fe5c9c3de64d0d6
madson <madsonsilva2005@gmail.com>
2023-06-29 10:20:05 -0300

    adicionado router do bairro

---
+++ src/Aplication/Routes/Bairro.route.ts
@@ -0,0 +1,6 @@
+import { Router } from "express";
+import { getBairroController } from "../Domains/Bairro/Usecases/Get/GetBairro.factory";
+
+const routerBairro = Router();
+
+routerBairro.get("/", getBairroController.handle);
