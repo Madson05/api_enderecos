4fbad56ef3a43cefee2d9aee4d29af9b6c432df0
madson <madsonsilva2005@gmail.com>
2023-06-28 21:42:09 -0300

    feat: adicionado metodo de criação no repositoryMunicipio

--- src/Infra/repositories/Municipio.repository.ts
+++ src/Infra/repositories/Municipio.repository.ts
@@ -1,3 +1,4 @@
+import MunicipioEntity from "../../Aplication/Entities/MunicipioEntity";
 import getConnection from "../database/connectionDB";
 
 class MunicipioRepository{
@@ -14,6 +15,22 @@ class MunicipioRepository{
       const result = await connection.execute(sql);
       await connection.commit();
       return result.rows;
+    
+    }finally{
+      if(connection){
+        await connection.close();
+      }
+    }
+  }
+
+  async create(municipio: MunicipioEntity){
+    let connection;
+    try{
+      connection = await getConnection();
+      const sql = `INSERT INTO TB_MUNICIPIO (CODIGO_UF, CODIGO_MUNICIPIO, NOME_MUNICIPIO, status) VALUES (:codigo_uf, :codigo_municipio, :nome_municipio, :status)`;
+      const result = await connection.execute(sql, [municipio.getCodigoUF(), municipio.getCodigoMunicipio(), municipio.getNome(), municipio.getStatus()]);
+      await connection.commit();
+      return await this.get("")
     
     }finally{
       if(connection){
