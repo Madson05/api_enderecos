03f2da28814d8eb4d66e4c52bb26cccbacc907e3
madson <madsonsilva2005@gmail.com>
2023-06-28 21:46:42 -0300

    feat: adicionado service de criação de municipio

---
+++ src/Aplication/Domains/Municipio/UseCases/Create/CreateMunicipio.service.ts
@@ -0,0 +1,15 @@
+import MunicipioRepository from "../../../../../Infra/repositories/Municipio.repository";
+import MunicipioEntity from "../../../../Entities/MunicipioEntity";
+import { refactorResult } from "../../Utils/RefactorResult";
+import { CreateMunicipioType } from "./Schemas/CreateMunicipio.schema";
+
+class CreateMunicipioService {
+  constructor(private readonly municipioRepository: MunicipioRepository) {}
+
+  async execute(data: CreateMunicipioType): Promise<any> {
+    const municipio = new MunicipioEntity(data.codigo_municipio, data.codigo_UF, data.nome, data.status);
+    const resultSet = await this.municipioRepository.create(municipio);
+
+    return refactorResult(resultSet);
+  }
+}
