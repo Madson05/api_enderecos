b07d114f57f96c2aed42e45c9f7c7931ea110739
madson <madsonsilva2005@gmail.com>
2023-06-28 21:58:12 -0300

    feat: adicionada rota para criação de municipio

--- src/Aplication/Routes/Municipio.route.ts
+++ src/Aplication/Routes/Municipio.route.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { getMunicipioController } from "../Domains/Municipio/UseCases/Get/GetMunicpio.factory";
+import { createMunicipioController } from "../Domains/Municipio/UseCases/Create/CreateMunicipio.factory";
 
 const routerMunicipio = Router();
 
 routerMunicipio.get("/", getMunicipioController.handle);
+routerMunicipio.post("/", createMunicipioController.handle);
 
 export default routerMunicipio;
