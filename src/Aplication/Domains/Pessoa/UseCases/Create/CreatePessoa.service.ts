c633da53a2bbe283444149b4a37b77773f7ee8cc
madson <madsonsilva2005@gmail.com>
2023-06-30 18:01:10 -0300

    fix: corrigido problema na geração de sequence

--- src/Aplication/Domains/Pessoa/UseCases/Create/CreatePessoa.service.ts
+++ src/Aplication/Domains/Pessoa/UseCases/Create/CreatePessoa.service.ts
@@ -12,9 +12,10 @@ class CreatePessoaService{
     execute = async (data: CreateUsuarioType): Promise<any> => {
         // separar a data em duas entidades uma com o atributo endereço e outtra com o restante.
         const codigoPessoa = await getNextSequence("SEQUENCE_PESSOA");
+        const codigoBairro = await getNextSequence("SEQUENCE_BAIRRO");
         const pessoa = new PessoaEntity(codigoPessoa, data.nome, data.sobrenome, data.idade, data.login, data.senha, data.status);
         const enderecos: EnderecoEntity[] = data.enderecos.map((endereco) => {
-            return new EnderecoEntity(await getNextSequence("SEQUENCE_ENDERECO"), codigoPessoa, endereco.codigoBairro, endereco.nomeRua, endereco.numero, endereco.complemento, endereco.cep));
+            return new EnderecoEntity(codigoBairro, codigoPessoa, endereco.codigoBairro, endereco.nomeRua, endereco.numero, endereco.complemento, endereco.cep)});
         const result = await this.pessoaRepository.create(pessoa, enderecos);
         return result;
     }
