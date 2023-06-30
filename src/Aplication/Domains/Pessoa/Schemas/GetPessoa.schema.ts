c7355e1bce851982364cb67063f083fd407d10eb
madson <madsonsilva2005@gmail.com>
2023-06-30 13:55:56 -0300

    feat: adicionado schema para busca de pessoa

---
+++ src/Aplication/Domains/Pessoa/Schemas/GetPessoa.schema.ts
@@ -0,0 +1,9 @@
+import zod from "zod";
+
+export const GetPessoaSchema = zod.object({
+  codigoPessoa: zod.string().optional(),
+  login: zod.string().email("Email inválido").optional(),
+  status: zod.string().min(1).max(2).optional(),
+});
+
+export type GetPessoaSchemaType = zod.infer<typeof GetPessoaSchema>;
