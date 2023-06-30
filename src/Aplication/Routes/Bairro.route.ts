cf42a00d9fc5999595625ec7f8d6d11d2a83cfc1
madson <madsonsilva2005@gmail.com>
2023-06-29 22:19:15 -0300

    adicionada rota de edição de bairro.

--- src/Aplication/Routes/Bairro.route.ts
+++ src/Aplication/Routes/Bairro.route.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { getBairroController } from "../Domains/Bairro/Usecases/Get/GetBairro.factory";
 import { createBairroController } from "../Domains/Bairro/Usecases/Create/CreateBairro.factory";
+import { updateBairroController } from "../Domains/Bairro/Usecases/Update/UpdateBairro.factory";
 
 const routerBairro = Router();
 
 routerBairro.get("/", getBairroController.handle);
 routerBairro.post("/", createBairroController.handle);
+routerBairro.put("/", updateBairroController.handle);
 
 export default routerBairro;
