590018c4b1d0f4392205271f6c60828e1a97b4e0
madson <madsonsilva2005@gmail.com>
2023-06-30 16:34:02 -0300

    fix: corrigido problema no acesso ao service

--- src/Aplication/Domains/Pessoa/GetPessoa.controller.ts
+++ src/Aplication/Domains/Pessoa/GetPessoa.controller.ts
@@ -5,7 +5,7 @@ import { GetPessoaSchema } from "./Schemas/GetPessoa.schema";
 class GetPessoaController{
   constructor(private readonly getPessoaService: GetPessoaService) {}
 
-  async handle(request: Request, response: Response): Promise<Response> {
+  handle = async (request: Request, response: Response): Promise<Response> => {
     const data = GetPessoaSchema.parse(request.query);
     const result = await this.getPessoaService.execute(data);
     return response.status(200).json(result);
