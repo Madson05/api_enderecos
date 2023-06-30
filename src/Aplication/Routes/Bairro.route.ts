6c70504c778e42f4ac82283b5b83f83d0c6279fa
madson <madsonsilva2005@gmail.com>
2023-06-29 21:05:51 -0300

    feat: adicionado rota de criação de bairro

--- src/Aplication/Routes/Bairro.route.ts
+++ src/Aplication/Routes/Bairro.route.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { getBairroController } from "../Domains/Bairro/Usecases/Get/GetBairro.factory";
+import { createBairroController } from "../Domains/Bairro/Usecases/Create/CreateBairro.factory";
 
 const routerBairro = Router();
 
 routerBairro.get("/", getBairroController.handle);
+routerBairro.post("/", createBairroController.handle);
 
 export default routerBairro;
