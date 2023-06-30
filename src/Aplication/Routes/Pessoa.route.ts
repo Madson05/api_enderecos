f986bcaf0cc4c45ce861a8f3737a70cbe3b08374
madson <madsonsilva2005@gmail.com>
2023-06-30 18:10:37 -0300

    feat: implementada rota de criação de pessoa

--- src/Aplication/Routes/Pessoa.route.ts
+++ src/Aplication/Routes/Pessoa.route.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { getPessoaController } from "../Domains/Pessoa/UseCases/Get/GetPessoa.factory";
+import { createPessoaController } from "../Domains/Pessoa/UseCases/Create/CreatePessoa.factory";
 
 const routerPessoa = Router();
 
 routerPessoa.get("/", getPessoaController.handle);
+routerPessoa.post("/", createPessoaController.handle);
 
 export default routerPessoa;
