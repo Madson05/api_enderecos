10cd27669f3a67943b1e5d516946a8f2d3f84d54
madson <madsonsilva2005@gmail.com>
2023-06-30 16:31:05 -0300

    feat: integrado routerPessoa no router geral da aplicação através da rota /pessoa

--- src/Aplication/Routes/Router.ts
+++ src/Aplication/Routes/Router.ts
@@ -2,11 +2,13 @@ import {Router} from "express"
 import routerUF from "./UF.route";
 import routerMunicipio from "./Municipio.route";
 import routerBairro from "./Bairro.route";
+import routerPessoa from "./Pessoa.route";
 
 const router = Router();
 
 router.use("/uf", routerUF)
 router.use("/municipio", routerMunicipio)
 router.use("/bairro", routerBairro)
+router.use("/pessoa", routerPessoa)
 
 export default router;
