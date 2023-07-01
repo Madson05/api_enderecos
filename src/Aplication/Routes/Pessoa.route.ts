45cc6e002ed852a7bab1a84b88af38c462f2e259
madson <madsonsilva2005@gmail.com>
2023-07-01 15:08:47 -0300

    criada rota para exclusão de pessoa

--- src/Aplication/Routes/Pessoa.route.ts
+++ src/Aplication/Routes/Pessoa.route.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import { getPessoaController } from "../Domains/Pessoa/UseCases/Get/GetPessoa.factory";
 import { createPessoaController } from "../Domains/Pessoa/UseCases/Create/CreatePessoa.factory";
 import { updatePessoaController } from "../Domains/Pessoa/UseCases/Update/UpdatePessoa.factory";
+import { deletePessoaController } from "../Domains/Pessoa/UseCases/Delete/DeletePessoa.factory";
 
 const routerPessoa = Router();
 
 routerPessoa.get("/", getPessoaController.handle);
 routerPessoa.post("/", createPessoaController.handle);
 routerPessoa.put("/", updatePessoaController.handle);
+routerPessoa.delete("/:codigoPessoa", deletePessoaController.handle);
 
 export default routerPessoa;
