103a108bfd33eb478e8d4fd9b1f51bd3bf8272e2
madson <madsonsilva2005@gmail.com>
2023-06-30 22:07:02 -0300

    feat: adicionada rota para edição de pessoa

--- src/Aplication/Routes/Pessoa.route.ts
+++ src/Aplication/Routes/Pessoa.route.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { getPessoaController } from "../Domains/Pessoa/UseCases/Get/GetPessoa.factory";
 import { createPessoaController } from "../Domains/Pessoa/UseCases/Create/CreatePessoa.factory";
+import { updatePessoaController } from "../Domains/Pessoa/UseCases/Update/UpdatePessoa.factory";
 
 const routerPessoa = Router();
 
 routerPessoa.get("/", getPessoaController.handle);
 routerPessoa.post("/", createPessoaController.handle);
+routerPessoa.put("/", updatePessoaController.handle);
 
 export default routerPessoa;
