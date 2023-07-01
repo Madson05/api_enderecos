5516f9e403be57d73eb02759fb479f8132c4725a
madson <madsonsilva2005@gmail.com>
2023-07-01 10:54:24 -0300

    refactor: removido codigo desnecessario

--- src/Aplication/Domains/Pessoa/Utils/RefactorResult.ts
+++ src/Aplication/Domains/Pessoa/Utils/RefactorResult.ts
@@ -6,18 +6,15 @@ import PessoaEntity from "../../../Entities/PessoaEntity";
 export const refactorResult = (result: any, data?: GetPessoaType) => {
   
   if (result && result.length > 0) {
-    if (data && (data.codigoPessoa !== undefined)) {
+    if (data) {
       const dataPessoa = result[0] as unknown[];
       const pessoa = new PessoaEntity(Number(dataPessoa[0]), String(dataPessoa[1]), String(dataPessoa[2]), Number(dataPessoa[3]), String(dataPessoa[4]), String(dataPessoa[5]), Number(dataPessoa[6]));
-      console.log("oi")
+      
       return {...pessoa, enderecos: []};
     } else {
-      for (const item in result) {
-        const dataPessoa = result[item] as unknown[];
-        const pessoa = new PessoaEntity(Number(dataPessoa[0]), String(dataPessoa[1]), String(dataPessoa[2]), Number(dataPessoa[3]), String(dataPessoa[4]), String(dataPessoa[5]), Number(dataPessoa[6]));
-        result[item] = {...pessoa, enderecos: []};
-      }
-    }
     return result;
+    }
   }
+  return result;
+
 }
